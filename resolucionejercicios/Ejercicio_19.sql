932a4e86ee465b20e340f293dcdadb7913c616d6
Lorena Garcia CF <g_fgl@live.com.ar>
2023-03-07 17:56:16 -0300

    ejercico base de Datos

---
+++ resolucionejercicios/Ejercicio_19.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-03-2023 a las 17:07:24
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ejerciciosmod4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `sexo` varchar(1) DEFAULT NULL,
+  `nivel` tinyint(4) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `marca` varchar(20) DEFAULT NULL,
+  `compania` varchar(20) DEFAULT NULL,
+  `saldo` float DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `Usuarios`
+--
+
+
+
+--Ejercicio 19_Listar el login y teléfono de los usuarios con compañía telefónica que no sea TELCEL o IUSACELL
+INSERT INTO `Usuarios` (`usuario`, `telefono`) VALUES
+('OSC4677', '655-143-4181'),
+('JOS7086', '655-143-3922'),
+('LUI6115', '655-137-1279'),
+('DAN2832', '655-145-2586'),
+('BLA9739', '655-330-3871'),
+('JES4752', '655-143-6861'),
+('DIA6570', '655-143-3952'),
+('VAL6882', '655-137-4253'),
+('BRE8106', '655-100-1351'),
+('ELP2984', '655-145-9938'),
+('LET4015', '655-143-4019'),
+('LUI1076', '655-100-5085'),
+('HUG5441', '655-137-3935');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
